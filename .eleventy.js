e6f6c8495e9969a1e4d434b410c89f8eacf14f0c
Joey Sardella <josephsardella@gmail.com>
2023-11-06 23:57:11 -0800

    fixed pass through filter for cms

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/admin");
 
     return {
         dir: {
